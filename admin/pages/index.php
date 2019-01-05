b519675c29dbb511437616dc7316d8df4f874b36
munaALB <boamuna@yahoo.com>
2019-01-05 11:12:25 -0500

    the admin side - the login page ( frontend)

---
+++ admin/pages/index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+	<link rel="icon" type="image/png" href="../assets/img/favicon.png">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>
+	  تسجيل الدخول
+	</title>
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+	<!--     Fonts and icons     -->
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  
+	<!-- CSS Files -->
+	<link href="../assets/css/material-dashboard.css?v=2.1.0" rel="stylesheet" />
+	<link href="../assets/css/material-dashboard-rtl.css?v=1.1" rel="stylesheet" />
+	<!-- CSS Just for demo purpose, don't include it in your project -->
+	<link href="../assets/demo/demo.css" rel="stylesheet" />
+</head>
+<body  >
+	
+	<div class="limiter">
+		<div class="container-login100" >
+			<div class="wrap-login100" >
+				<div class="login100-pic js-tilt" data-tilt >
+					<img src="../assets/img/img-01.png" alt="IMG">
+				</div>
+
+				<form class="login100-form validate-form" style="direction : rtl">
+					<span class="login100-form-title">
+						تسجيل دخول 
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="email"  >
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true" style=" padding-right: 15px;"></i>
+						</span> 
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="password" name="pass" >
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true" style=" padding-right: 15px;"></i>
+						</span>
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							تسجيل دخول
+						</button>
+					</div>
+
+					
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	
+
+	
+	<script src="../assets/js/core/jquery.min.js"></script>
+	<script src="../assets/js/core/popper.min.js"></script>
+	<script src="../assets/js/core/bootstrap-material-design.min.js"></script>
+	<script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+	<!-- Plugin for the momentJs  -->
+	<script src="../assets/js/plugins/moment.min.js"></script>
+	<!--  Plugin for Sweet Alert -->
+	<script src="../assets/js/plugins/sweetalert2.js"></script>
+	<!-- Forms Validations Plugin -->
+	<script src="../assets/js/plugins/jquery.validate.min.js"></script>
+	<!-- Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
+	<script src="../assets/js/plugins/jquery.bootstrap-wizard.js"></script>
+	<!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
+	<script src="../assets/js/plugins/bootstrap-selectpicker.js"></script>
+	<!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
+	<script src="../assets/js/plugins/bootstrap-datetimepicker.min.js"></script>
+	<!--  DataTables.net Plugin, full documentation here: https://datatables.net/  -->
+	<script src="../assets/js/plugins/jquery.dataTables.min.js"></script>
+	<!--	Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
+	<script src="../assets/js/plugins/bootstrap-tagsinput.js"></script>
+	<!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+	<script src="../assets/js/plugins/jasny-bootstrap.min.js"></script>
+	<!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
+	<script src="../assets/js/plugins/fullcalendar.min.js"></script>
+	<!-- Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
+	<script src="../assets/js/plugins/jquery-jvectormap.js"></script>
+	<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+	<script src="../assets/js/plugins/nouislider.min.js"></script>
+	<!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support SweetAlert -->
+	<script src="../assets/js/core/core.js"></script>
+	<!-- Library for adding dinamically elements -->
+	<script src="../assets/js/plugins/arrive.min.js"></script>
+	<!--  Google Maps Plugin    -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+	<!-- Chartist JS -->
+	<script src="../assets/js/plugins/chartist.min.js"></script>
+	<!--  Notifications Plugin    -->
+	<script src="../assets/js/plugins/bootstrap-notify.js"></script>
+	<!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+	<script src="../assets/js/material-dashboard.js" type="text/javascript"></script>
+	<!-- Material Dashboard DEMO methods, don't include it in your project! -->
+	<script src="../assets/demo/demo.js"></script>
+
+</body>
+</html>
